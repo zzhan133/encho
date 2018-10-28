7dea12038070c0c7ec89db5f5f33786e62dc6d74
zzhan133 <tmzhangzhe@gmail.com>
2018-10-28 01:05:38 -0700

    add pom file

---
+++ src/main/java/com/sz/learn/App.java
@@ -0,0 +1,13 @@
+package com.sz.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
